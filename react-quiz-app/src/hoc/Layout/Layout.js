3d743f9a0594044eedadffbe005bde8e134a3f1c
SviatoslavMJS <69400261+SviatoslavMJS@users.noreply.github.com>
2021-06-21 15:52:50 +0300

    create components Drawer and Backdrop

--- react-quiz-app/src/hoc/Layout/Layout.js
+++ react-quiz-app/src/hoc/Layout/Layout.js
@@ -1,11 +1,38 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle"
 import classes from "./Layout.module.css"
+import Drawer from "../../components/Navigation/Drawer/Drawer"
 
-class Layout extends Component{
-    render(){
-        return(
+class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    menuOnCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
+    render() {
+        return (
             <div className={classes.Layout}>
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuOnCloseHandler}
+                />
 
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
                 <main>
                     {this.props.children}
                 </main>
