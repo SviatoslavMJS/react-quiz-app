27aed68e252c81e97d373ab39fd8f5196e249da0
SviatoslavMJS <69400261+SviatoslavMJS@users.noreply.github.com>
2021-07-15 19:33:06 +0300

    addLoader

--- react-quiz-app/src/containers/QuizList/QuizList.js
+++ react-quiz-app/src/containers/QuizList/QuizList.js
@@ -2,15 +2,17 @@ import React, { Component } from "react"
 import { NavLink } from "react-router-dom"
 import classes from "./QuizList.module.css"
 import axios from "axios"
+import Loader from "../../components/UI/Loader/Loader"
 
 class QuizList extends Component {
 
     state = {
         quizes: [],
+        loading: true
     }
 
     renderQuizes = () => {
-        return this.state.quizes.map( quiz => {
+        return this.state.quizes.map(quiz => {
             return (
                 <li key={quiz.id}>
                     <NavLink
@@ -35,7 +37,8 @@ class QuizList extends Component {
             })
 
             this.setState({
-                quizes
+                quizes,
+                loading: false
             })
 
         } catch (error) {
@@ -50,10 +53,14 @@ class QuizList extends Component {
             <div className={classes.QuizList}>
                 <div>
                     <h1>Список Тестов</h1>
+                    {
+                        this.state.loading
+                            ? <Loader />
+                            : <ul>
+                                {this.renderQuizes()}
+                            </ul>
+                    }
 
-                    <ul>
-                        {this.renderQuizes()}
-                    </ul>
                 </div>
             </div>
         )
